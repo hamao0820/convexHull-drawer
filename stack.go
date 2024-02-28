c1f2ee31f1974b44a895cfafe12c00be2c4aeac2
浜田　俊祐 <hamashun0820@icloud.com>
2024-02-29 08:49:23 +0900

    add: stack

---
+++ stack.go
@@ -0,0 +1,35 @@
+package main
+
+type Stack[T any] struct {
+	stack []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{stack: make([]T, 0)}
+}
+
+func (s *Stack[T]) Push(x T) {
+	s.stack = append(s.stack, x)
+}
+
+func (s *Stack[T]) Pop() T {
+	x := s.stack[len(s.stack)-1]
+	s.stack = s.stack[:len(s.stack)-1]
+	return x
+}
+
+func (s *Stack[T]) Top() T {
+	return s.stack[len(s.stack)-1]
+}
+
+func (s *Stack[T]) Len() int {
+	return len(s.stack)
+}
+
+func (s *Stack[T]) Empty() bool {
+	return len(s.stack) == 0
+}
+
+func (s *Stack[T]) Slice() []T {
+	return s.stack
+}
